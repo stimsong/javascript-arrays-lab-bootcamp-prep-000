4fc755d760b4f8c5a0540444cfade59501c1c521
Greg Stimson <stimsong@yahoo.com>
2018-01-15 19:07:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const app = "I don't do much."
 
 function destructivelyAppendKitten(name) {
   return kittens.push(name);
-//  return kittens;
 }
 
-//function destructivelyPrependKitten(name) {
-//  kittens.unshift(name);
-//}
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
